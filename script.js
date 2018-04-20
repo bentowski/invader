9fb72cd521012a0189860526fe2cca9d54710cf9
Unknown <benjamin.baudry@hotmail.com>
2018-04-20 15:51:22 +0200

    tests tirs
    tests tirs mais pas de modif majeure

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function infosClavier(e) {
     var number = Number(e.keyCode);
   switch (number){
     case 32:
-    mvmt()
+    shoot()
     break;
     case 37:
     gauche()
@@ -31,45 +31,46 @@ const ctx = vague.getContext('2d')
 const ship=document.getElementById("ship")
 const shipCtx=ship.getContext("2d")
 var long=0
-  , xs=l/2-85/2
-  , ys=10
+  , xVaisseau=l/2-85/2
+  , yVaisseau=10
   , rang=0
-  , ye=25
-  , xe=""
+  , yVague=25
+  , xVague=""
   , Droite=0
   , Gauche=0
-  , vitesse=30 // vitesse vaisseau
-  , vert=vitesse+1
-  , noir=vitesse+2
+  , vitesseVaisseau=30 // vitesse vaisseau
+  , vert=vitesseVaisseau+1
+  , noir=vitesseVaisseau+2
   , xred=150+85
   , xblack=150
   , xf=""
   , xr=""
-  , vitessev=10 //vitesse vague
-  , rouge=vitessev+1
-  , Noir=vitessev+2
+  , vitesseVague=10 //vitesse vague
+  , rouge=vitesseVague+1
+  , Noir=vitesseVague+2
   , deplacementDroite=true
   , mvmtv=0
+  , vitesseTir=5
 
 function init(){
   while(long < 85){
         shipCtx.fillStyle="green"
-        shipCtx.fillRect(xs, ys, 1, 50)
-        xs=xs+1
+        shipCtx.fillRect(xVaisseau, yVaisseau, 1, 50)
+        xVaisseau=xVaisseau+1
     long++
   }
 
   while(rang < 5){
-    xe=150
+    xVague=150
     for(i=0; i<10; i++){
         ctx.fillStyle="red"
-        ctx.fillRect(xe, ye, 85, 50)
-        xe=xe+170
+        ctx.fillRect(xVague, yVague, 85, 50)
+        xVague=xVague+170
     }
-    ye=ye+90
+    yVague=yVague+90
     rang++
   }
-  ye=25
+  yVague=25
   setInterval(move,500)
 }
 
@@ -92,24 +93,24 @@ function move(){
       xred+=95
       xblack-=75
     }
-      xred+=vitessev
-      xblack+=vitessev
+      xred+=vitesseVague
+      xblack+=vitesseVague
       rang=0
       while(rang < 5){
         xf=xred
         xr=xblack
         for(i=0; i<10; i++){
             ctx.fillStyle="red"
-            ctx.fillRect(xf-10, ye, rouge, 50)
+            ctx.fillRect(xf-10, yVague, rouge, 50)
             xf=xf+170
             ctx.fillStyle="black"
-            ctx.fillRect(xr-10, ye, Noir, 50)
+            ctx.fillRect(xr-10, yVague, Noir, 50)
             xr=xr+170
         }
-        ye=ye+90
+        yVague=yVague+90
         rang++
       }
-      ye=25
+      yVague=25
       mvmtv=0
       if(xf >= vagues.width){
         deplacementDroite=false
@@ -121,24 +122,24 @@ function move(){
      xred-=95
      xblack+=75
    }
-     xred-=vitessev
-     xblack-=vitessev
+     xred-=vitesseVague
+     xblack-=vitesseVague
      rang=0
      while(rang < 5){
        xf=xred
        xr=xblack
        for(i=0; i<10; i++){
          ctx.fillStyle="red"
-         ctx.fillRect(xf+11, ye, rouge, 50)
+         ctx.fillRect(xf+11, yVague, rouge, 50)
          xf=xf+170
          ctx.fillStyle="black"
-         ctx.fillRect(xr+10, ye, Noir, 50)
+         ctx.fillRect(xr+10, yVague, Noir, 50)
          xr=xr+170
        }
-       ye=ye+90
+       yVague=yVague+90
        rang++
      }
-     ye=25
+     yVague=25
      mvmtv=1
      if(xr <= 1900){
        deplacementDroite=true
@@ -148,50 +149,60 @@ function move(){
 //===============ship=====================
 
 function droite(){
-  if(xs < l-100){
+  if(xVaisseau < l-100){
         if (Gauche===1){
           shipCtx.fillStyle="green"
-          shipCtx.fillRect(xs-9, ys, vert, 50)
-          var left=xs-89
+          shipCtx.fillRect(xVaisseau-9, yVaisseau, vert, 50)
+          var left=xVaisseau-89
           shipCtx.fillStyle="black"
-          shipCtx.fillRect(left,ys,noir,50)
+          shipCtx.fillRect(left,yVaisseau,noir,50)
         }
         Gauche=0
         shipCtx.fillStyle="green"
-        shipCtx.fillRect(xs, ys, vert, 50)
-        var left=xs-86
+        shipCtx.fillRect(xVaisseau, yVaisseau, vert, 50)
+        var left=xVaisseau-86
         shipCtx.fillStyle="black"
-        shipCtx.fillRect(left,ys,noir,50)
-        xs+=vitesse
+        shipCtx.fillRect(left,yVaisseau,noir,50)
+        xVaisseau+=vitesseVaisseau
         Droite=1
     }
 }
 
 function gauche(){
-  if (xs > (100+85)){
+  if (xVaisseau > (100+85)){
         if (Droite===1){
           shipCtx.fillStyle="green"
-          shipCtx.fillRect(xs-86, ys, vert, 50)
-          var right=xs-9
+          shipCtx.fillRect(xVaisseau-86, yVaisseau, vert, 50)
+          var right=xVaisseau-9
           shipCtx.fillStyle="black"
-          shipCtx.fillRect(right,ys,noir,50)
+          shipCtx.fillRect(right,yVaisseau,noir,50)
         }
         Droite=0
         shipCtx.fillStyle="green"
-        shipCtx.fillRect(xs-95, ys, vert, 50)
-        var right=xs-10
+        shipCtx.fillRect(xVaisseau-95, yVaisseau, vert, 50)
+        var right=xVaisseau-10
         shipCtx.fillStyle="black"
-        shipCtx.fillRect(right,ys,noir,50)
-        xs-=vitesse
+        shipCtx.fillRect(right,yVaisseau,noir,50)
+        xVaisseau-=vitesseVaisseau
         Gauche=1
     }
 }
 
-/*function tir(){
-  ys=60
-  while (ys<h){
-    shipCtx.fillStyle="yellow"
-    shipCtx.fillRect(xs-85/2, ys, 5, 20)
-    ys+=1
-  }
-}*/
+//==============tir============
+// var xTir
+// var yTir
+//
+// function shoot() {
+//   xTir=xVaisseau
+//   yTir=yVaisseau
+//   console.log(yTir);
+//   shipCtx.fillStyle="yellow"
+//   shipCtx.fillRect(xTir-84/2-5, yTir, 10, 50)
+//   console.log(yTir);
+// }
+//
+// function deplacementTir() {
+//     shipCtx.fillStyle="yellow"
+//     shipCtx.fillRect(xTir-84/2-5, yTir, 10, 50)
+//     console.log(yTir);
+// }
