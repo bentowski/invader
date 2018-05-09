f966873559f2148d2d3defb621e75eee1d8e98b4
Unknown <benjamin.baudry@hotmail.com>
2018-05-09 15:45:21 +0200

    selection ennemis
    chaque enemi est devenu indépendant et peu être supprimé
    augmentation des fps pour fluidifier l'image (réduction de la vitesse+augmentation du setInterval)

--- scriptES6.js
+++ scriptES6.js
@@ -25,8 +25,8 @@ l=window.innerWidth
 h=window.innerHeight
 const vague = document.getElementById("vagues")
 const ctx = vague.getContext('2d')
-const ship = document.getElementById("ship")
-const shipCtx = ship.getContext('2d')
+// const ship = document.getElementById("ship")
+// const ctx = ship.getContext('2d')
 let widthElements=0
   , finalWidthElements=85
   , heightElements=50
@@ -35,7 +35,7 @@ let widthElements=0
   , spaceWaveElements=170
   , spaceWaveElementsHeight=90
   , xShip=l/2-finalWidthElements/2
-  , yShip=0
+  , yShip=800
   , rowWave=0
   , finalRowWave=5
   , xWave=150
@@ -46,12 +46,18 @@ let widthElements=0
   , black=shipSpeed+2
   , waveMoveRight=true
   , previousWaveMove=""
-  , shootSpeed=5
-  , waveSpeed=20
+  , shootSpeed=20
+  , waveSpeed=1
   , red=waveSpeed+2
   , Black=waveSpeed+2
   , xRed=xWave+finalWidthElements
   , xBlack=xWave
+  , enemiNumber=[]
+  , number=0
+  , weaponHeight=30
+  , weaponWidth=5
+  , yWeapon=vagues.height-weaponHeight
+  , xWeapon
 
 //=============initialisation===========
 
@@ -59,8 +65,8 @@ var init = () => {
   let row=rowWave
   let y=yWave
   while (widthElements<finalWidthElements) {
-    shipCtx.fillStyle="green"
-    shipCtx.fillRect(xShip, yShip, columnWidth, heightElements)
+    ctx.fillStyle="green"
+    ctx.fillRect(xShip, yShip, columnWidth, heightElements)
     xShip++
     widthElements++
   }
@@ -69,12 +75,14 @@ var init = () => {
     for (i=0; i<waveElements; i++){
       ctx.fillStyle="red"
       ctx.fillRect(x, y, finalWidthElements, heightElements)
+      enemiNumber[number]={id:number, active:true}
       x+=spaceWaveElements
+      number++
     }
     y+=spaceWaveElementsHeight
     row++
   }
- setInterval(move,500)
+ setInterval(move,5)
 }
 
 //===========Wave Move===================
@@ -82,62 +90,48 @@ var init = () => {
 var move = ()=>{waveMoveRight ? mvmtR() : mvmtL()}
 
 var mvmtR = ()=>{
+  xWave+=waveSpeed
   let y=yWave
-  let row=rowWave
-  console.log("width"+finalWidthElements);
-  console.log("previous=================="+previousWaveMove);
-  previousWaveMove ? (xRed+=finalWidthElements-columnWidth-waveSpeed, xBlack-=finalWidthElements+waveSpeed) : false
-  xRed+=waveSpeed
-  xBlack+=waveSpeed
-
-  while (row<finalRowWave){
-    let xa=xRed
-    let xb=xBlack
-    for(i=0; i<waveElements; i++){
-      ctx.fillStyle="red"
-      ctx.fillRect(xa-waveSpeed, y, red, heightElements)
-      xa+=spaceWaveElements
-      console.log("xar"+i+xa);
-      ctx.fillStyle="black"
-      ctx.fillRect(xb-waveSpeed, y, Black, heightElements)
-      xb+=spaceWaveElements
-      console.log("xbr"+i+xb)
+  let x=xWave
+  for(i=0;i<enemiNumber.length;i++){
+    if(i===10 || i===20 || i===30 || i===40){
+      y+=spaceWaveElementsHeight
+      x=xWave
     }
-    y+=spaceWaveElementsHeight
-    row++
-
-    (xa>=vagues.width) ? waveMoveRight=false : waveMoveRight=true
-  }
-previousWaveMove=false;
-console.log(previousWaveMove);
-}
-
-var mvmtL = ()=>{
-  let y=yWave;
-  let row=rowWave;
-  !previousWaveMove ? (xRed-=finalWidthElements-waveSpeed-columnWidth, xBlack+=finalWidthElements+waveSpeed) : false;
-  xRed-=waveSpeed;
-  xBlack-=waveSpeed;
-  while(row<finalRowWave){
-    let xa=xRed;
-    let xb=xBlack;
-    for(i=0; i<waveElements; i++){
-      ctx.fillStyle="red";
-      ctx.fillRect(xa-waveSpeed, y, red, heightElements);
-      xa+=spaceWaveElements;
-      console.log("xal"+i+xa)
-      ctx.fillStyle="black";
-      ctx.fillRect(xb-waveSpeed, y, Black, heightElements);
-      xb+=spaceWaveElements;
-      console.log("xbl"+i+xb)
+      if(enemiNumber[i].active){
+        ctx.clearRect(x-waveSpeed,y, finalWidthElements, heightElements)
+        ctx.fillStyle="red"
+        ctx.fillRect(x,y,finalWidthElements,heightElements)
+      }
+      x+=spaceWaveElements
     }
-    y+=spaceWaveElementsHeight;
-    row++;
-    (xb<=1900) ? waveMoveRight=true : waveMoveRight=false;
+    if(xWave>=300){
+      waveMoveRight=false
+    }
   }
-  previousWaveMove=true;
-  console.log(previousWaveMove);
-}
+
+
+
+  var mvmtL = ()=>{
+    xWave-=waveSpeed
+    let y=yWave
+    let x=xWave
+    for(i=0;i<enemiNumber.length;i++){
+      if(i===10 || i===20 || i===30 || i===40){
+        y+=spaceWaveElementsHeight
+        x=xWave
+      }
+        if(enemiNumber[i].active){
+          ctx.clearRect(x+waveSpeed,y, finalWidthElements, heightElements)
+          ctx.fillStyle="red"
+          ctx.fillRect(x,y,finalWidthElements,heightElements)
+        }
+        x+=spaceWaveElements
+      }
+      if(xWave<=100){
+        waveMoveRight=true
+      }
+    }
 
 //==================ship=====================
 var right = ()=>{
@@ -149,37 +143,59 @@ var left = ()=>{
 }
 
 var rightMove = ()=>{
-  shipCtx.fillStyle="green"
-  shipCtx.fillRect(xShip, yShip, green, heightElements)
+  ctx.fillStyle="green"
+  ctx.fillRect(xShip, yShip, green, heightElements)
   let move=xShip-86
-  shipCtx.fillStyle="black"
-  shipCtx.fillRect(move,yShip,black,heightElements)
+  ctx.fillStyle="black"
+  ctx.fillRect(move,yShip,black,heightElements)
   xShip+=shipSpeed
   previousMove="right"
 }
 
 var hardRightMove = ()=>{
-  shipCtx.fillStyle="green"
-  shipCtx.fillRect(xShip-9, yShip, green, heightElements)
+  ctx.fillStyle="green"
+  ctx.fillRect(xShip-9, yShip, green, heightElements)
   let move=xShip-89
-  shipCtx.fillStyle="black"
-  shipCtx.fillRect(move,yShip,black,heightElements)
+  ctx.fillStyle="black"
+  ctx.fillRect(move,yShip,black,heightElements)
 }
 
 var leftMove = ()=>{
-  shipCtx.fillStyle="green"
-  shipCtx.fillRect(xShip-95, yShip, green, heightElements)
+  ctx.fillStyle="green"
+  ctx.fillRect(xShip-95, yShip, green, heightElements)
   let move=xShip-10
-  shipCtx.fillStyle="black"
-  shipCtx.fillRect(move,yShip,black,heightElements)
+  ctx.fillStyle="black"
+  ctx.fillRect(move,yShip,black,heightElements)
   xShip-=shipSpeed
   previousMove="left"
 }
 
 var hardLeftMove = ()=>{
-  shipCtx.fillStyle="green"
-  shipCtx.fillRect(xShip-86, yShip, green, heightElements)
+  ctx.fillStyle="green"
+  ctx.fillRect(xShip-86, yShip, green, heightElements)
   let move=xShip-9
-  shipCtx.fillStyle="black"
-  shipCtx.fillRect(move,yShip,black,heightElements)
+  ctx.fillStyle="black"
+  ctx.fillRect(move,yShip,black,heightElements)
 }
+
+//============shoot================
+
+var shoot=()=>{
+  xWeapon=xShip-finalWidthElements/2
+  yWeapon=yShip-heightElements
+  ctx.fillStyle="yellow"
+  ctx.fillRect(xWeapon, yWeapon , weaponWidth, weaponHeight)
+  setInterval(moveWeapon,500)
+  console.log("test1");
+}
+
+var moveWeapon = ()=>{
+  console.log("test");
+  ctx.clearRect(xWeapon,yWeapon,weaponWidth+1,weaponHeight)
+  yWeapon-=shootSpeed
+  ctx.fillStyle="yellow"
+  ctx.fillRect(xWeapon,yWeapon,weaponWidth,weaponHeight)
+  console.log(yWeapon);
+}
+
+var shOot = ()=> new shoot()
