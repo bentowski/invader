e1dcfab2b3b6a907df48ebc1c3abf7375db6a666
Unknown <benjamin.baudry@hotmail.com>
2018-05-14 16:37:16 +0200

    remise en forme du code + problème initialisation rélgé
    une seule initialisation peut être faite grâce à un if dans le swicth
    le code à été remis en forme et des commentaires pour les variables ont étés ajoutés

--- scriptES6.js
+++ scriptES6.js
@@ -1,82 +1,72 @@
-//==============Interaction================
+                  //==============INTERACTION================
 
 var infosClavier = e => {
   let number = Number(e.keyCode);
   switch (number) {
-    case 32:
-      shoot();
-      break;
-    case 37:
-      left();
-      break;
-    case 39:
-      right();
-      break;
     case 13:
-      init();
+     if(firstInit){
+       init()
+       firstInit=false
+     }
       break;
   }
 }
 
-var infosClavier2 = a => {
-  let number = Number(a.keyCode);
-  console.log(number);
-  switch (number) {
+var infosClavier2 = e => {
+  let number2 = Number(e.keyCode)
+  console.log(number2);
+  switch (number2) {
+    case 32:
+      shoot()
+      break;
     case 37:
-    stop()
+      leftMove()
       break;
     case 39:
-    stop()
+      rightMove()
       break;
   }
 }
-
 
 document.addEventListener("keydown", infosClavier);
-document.addEventListener("keyup", infosClavier2);
+document.addEventListener("keyup", infosClavier2)
 
-//===============variables=============
+                  //===============VARIABLES=============
+            //=====window dimensions=====
 l=window.innerWidth
 h=window.innerHeight
+            //=====canvas=====
 const vague = document.getElementById("vagues")
 const ctx = vague.getContext('2d')
-let widthElements=0
-  , finalWidthElements=85
+            //=====generals variables=====
+let finalWidthElements=85
   , heightElements=50
-  , columnWidth=1 //segments width for creation of Elements
-  , waveElements=10
-  , spaceWaveElements=170
-  , spaceWaveElementsHeight=90
-  , xShip=l/2-finalWidthElements/2
-  , yShip=800
-  , rowWave=0
-  , finalRowWave=5
-  , xWave=150
-  , yWave=25
-  , previousMove=""
-  , shipSpeed=1
-  , green=shipSpeed+1
-  , black=shipSpeed+2
-  , waveMoveRight=true
-  , previousWaveMove=""
-  , shootSpeed=20
-  , waveSpeed=1
-  , red=waveSpeed+2
-  , Black=waveSpeed+2
-  , xRed=xWave+finalWidthElements
-  , xBlack=xWave
-  , enemiNumber=[]
-  , number=0
-  , weaponHeight=30
-  , weaponWidth=5
-  , yWeapon=vagues.height-weaponHeight
-  , xWeapon
-  , moveShip=0
-  , previous=true
-  , leftShip
-  , rightShip
+  , firstInit=true //for first initializing
+            //===wave===
+  , spaceWaveElements=170 //space around elements horizontal
+  , spaceWaveElementsHeight=90 //space around rows
+  , waveElements=10 //number of elements by row
+  , finalRowWave=5 //number of row
+  , waveMoveRight=true //moving wave at right or left
+  , enemiNumber=[] //for id of elements
+  , waveSpeed=1 //for moving (about the setInterval at 10ms => 1*(60/10)move/s => 6move/s  )
+    //==initialisation==
+  , rowWave=0 //for counting rows
+  , number=0 //for id of elements
+  , xWave=150 //start pos x
+  , yWave=25  //start pos y
+            //===ship===
+  , xShip=l/2-finalWidthElements/2 //pos x Ship
+  , yShip=800 //pos y Ship
+  , shipSpeed=20 //moving Ship
+            //===weapon===
+  , weaponHeight=30 //hight of missils
+  , weaponWidth=5 //width of missils
+  , yWeapon=vagues.height-weaponHeight //start pos y
+  , xWeapon //position x (it's about the xShip)
+  , shootSpeed=20 //missil move
 
-//=============initialisation===========
+                    //=============INITIALIZING===========
 
 var init = () => {
   let row=rowWave
@@ -98,7 +88,7 @@ var init = () => {
  setInterval(move,10)
 }
 
-//===========Wave Move===================
+                    //===========WAVE MOVE===================
 
 var move = ()=>{waveMoveRight ? mvmtR() : mvmtL()}
 
@@ -123,8 +113,6 @@ var mvmtR = ()=>{
     }
   }
 
-
-
   var mvmtL = ()=>{
     xWave-=waveSpeed
     let y=yWave
@@ -146,48 +134,28 @@ var mvmtR = ()=>{
       }
     }
 
-
-//==================ship=====================
-
-var stop= ()=>{
-  clearInterval(leftShip)
-  clearInterval(rightShip)
-}
-
-var right = ()=>{
-  clearInterval(leftShip)
-  rightShip=setInterval(rightMove,10)
-}
-
-var left = ()=>{
-  clearInterval(rightShip)
-  leftShip=setInterval(leftMove,10)
-}
+                //==================SHIP=====================
 
 var rightMove = ()=>{
-  ctx.clearRect(xShip, yShip, finalWidthElements+1, heightElements)
-  // if(!previous){
-    xShip+=shipSpeed
-  // }
-  ctx.fillStyle="green"
-  ctx.fillRect(xShip, yShip, finalWidthElements, heightElements)
-  // previous=true
-  console.log(shipSpeed);
-  console.log(xShip);
+  if(xShip<=1850){
+    ctx.clearRect(xShip, yShip, finalWidthElements+1, heightElements)
+      xShip+=shipSpeed
+    ctx.fillStyle="green"
+    ctx.fillRect(xShip, yShip, finalWidthElements, heightElements)
+
+  }
 }
 
 var leftMove = ()=>{
-  ctx.clearRect(xShip, yShip, finalWidthElements+1, heightElements)
-  // if(previous){
-    xShip-=shipSpeed
-  // }
-  ctx.fillStyle="green"
-  ctx.fillRect(xShip, yShip, finalWidthElements, heightElements)
-  // previous=false
+  if(xShip>=100){
+    ctx.clearRect(xShip, yShip, finalWidthElements+1, heightElements)
+      xShip-=shipSpeed
+    ctx.fillStyle="green"
+    ctx.fillRect(xShip, yShip, finalWidthElements, heightElements)
+  }
 }
 
-
-//============shoot================
+                  //============SHOOT================
 
 var shoot=()=>{
   xWeapon=xShip-finalWidthElements/2
@@ -197,12 +165,9 @@ var shoot=()=>{
   setInterval(moveWeapon,500)
 }
 
-
 var moveWeapon = ()=>{
   ctx.clearRect(xWeapon,yWeapon,weaponWidth+1,weaponHeight)
   yWeapon-=shootSpeed
   ctx.fillStyle="yellow"
   ctx.fillRect(xWeapon,yWeapon,weaponWidth,weaponHeight)
 }
-
-// var shOot = ()=> {new shoot()}
