d8219a40c9f0ab5386e2470c7af3df87beb1fe2c
Unknown <benjamin.baudry@hotmail.com>
2018-05-16 14:20:22 +0200

    trainée vaisseau ok

--- scriptES6.js
+++ scriptES6.js
@@ -227,7 +227,7 @@
 
   var rightMove = ()=>{
     if(xShip<=1850){
-      ctx.clearRect(xShip, yShip, widthElements+1, heightElements)
+      ctx.clearRect(xShip-1, yShip, widthElements+1, heightElements)
         xShip+=shipSpeed
       ctx.fillStyle="green"
       ctx.fillRect(xShip, yShip, widthElements, heightElements)
